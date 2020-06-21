b52fa5a960c78b227c9af2d4815ad78e07a3bf34
Dmitriy Dubson <d.dubson@gmail.com>
2020-06-21 18:17:06 -0400

    Add .editorconfig; additional refactorings

--- src/main_scene.rs
+++ src/main_scene.rs
@@ -1,18 +1,20 @@
 use gtk::{Application, ApplicationWindow, ButtonBuilder,
-          BoxBuilder, ScrolledWindowBuilder, ScrolledWindow, Orientation, WidgetExt, ButtonExt,
+          BoxBuilder, Orientation, WidgetExt, ButtonExt,
           ContainerExt, GtkWindowExt, TextBufferExt};
 use glib::clone;
-use gtk::prelude::*;
 use std::process::Command;
+use crate::main_scene::process_container::ProcessUIContainer;
 
 struct WindowConfiguration {
+    title: &'static str,
     height: i32,
     width: i32,
 }
 
 const STD_WINDOW_CONFIG: WindowConfiguration = WindowConfiguration {
+    title: "mproc",
     height: 800,
-    width: 600
+    width: 600,
 };
 
 pub fn on_window_activate(app: &Application) {
@@ -22,30 +24,18 @@ pub fn on_window_activate(app: &Application) {
         .build();
     button.connect_clicked(clone!(@weak window => move |_| window.destroy()));
 
-    let process_container = process_container::create_process_ui_container();
-
-    let scrolled_window: ScrolledWindow = ScrolledWindowBuilder::new()
-        .min_content_height(400)
-        .min_content_width(600)
-        .child(&process_container.text_view)
-        .build();
+    let process_container: ProcessUIContainer = process_container::create_process_ui_container();
 
     let box_container = BoxBuilder::new()
         .orientation(Orientation::Vertical)
         .build();
-    box_container.add(&scrolled_window);
+    box_container.add(&process_container.scrolled_window);
     box_container.add(&button);
 
     window.add(&box_container);
-    window.set_title("mproc");
+    window.set_title(&STD_WINDOW_CONFIG.title);
     window.set_default_size(STD_WINDOW_CONFIG.width, STD_WINDOW_CONFIG.height);
     window.show_all();
-
-    // Listen for text view changes, auto-scroll as text is added.
-    process_container.text_view.connect_size_allocate(clone!(@weak scrolled_window => move |_,_| {
-        let adj = scrolled_window.get_vadjustment().unwrap();
-        adj.set_value(adj.get_upper() - adj.get_page_size());
-    }));
 
     let output = Command::new("lsof")
         .output()
@@ -64,20 +54,35 @@ pub fn on_window_activate(app: &Application) {
 }
 
 mod process_container {
-    use gtk::{TextView, TextBuffer, TextBufferBuilder};
+    use gtk::{TextView, TextBuffer, TextBufferBuilder, ScrolledWindow, ScrolledWindowBuilder, WidgetExt};
+    use glib::clone;
+    use gtk::prelude::*;
 
     pub struct ProcessUIContainer {
         pub text_buffer: TextBuffer,
         pub text_view: TextView,
+        pub scrolled_window: ScrolledWindow,
     }
 
     pub fn create_process_ui_container() -> ProcessUIContainer {
         let text_buffer: TextBuffer = TextBufferBuilder::new().build();
         let text_view: TextView = TextView::new_with_buffer(&text_buffer);
-        let process_ui_container = ProcessUIContainer {
+        let scrolled_window: ScrolledWindow = ScrolledWindowBuilder::new()
+            .min_content_height(400)
+            .min_content_width(600)
+            .child(&text_view)
+            .build();
+
+        // Listen for text view changes, auto-scroll as text is added.
+        text_view.connect_size_allocate(clone!(@weak scrolled_window => move |_,_| {
+            let adj = scrolled_window.get_vadjustment().unwrap();
+            adj.set_value(adj.get_upper() - adj.get_page_size());
+        }));
+
+        ProcessUIContainer {
             text_buffer,
             text_view,
-        };
-        process_ui_container
+            scrolled_window,
+        }
     }
 }
