4ceedc3ee7aaf86942a13f4f31ad1de897e00e4c
Dmitriy Dubson <d.dubson@gmail.com>
2020-09-04 11:13:42 -0400

    Add on application exit signal handler

--- src/application.rs
+++ src/application.rs
@@ -5,6 +5,7 @@ use crate::core::spawn_process::spawn_process;
 use crate::core::state::State;
 use crate::ui::gtk::styles_loader::initialize_styles;
 use crate::ui::main_window::MainWindow;
+use gio::ApplicationExt;
 use gtk::{Application, ApplicationWindow, GtkWindowExt};
 use log::{debug, info};
 use std::rc::Rc;
@@ -13,6 +14,7 @@ pub struct MprocApplication {
     state: Rc<State>,
     settings: AppSettings,
     main_window: MainWindow,
+    gtk_application: gtk::Application,
 }
 
 impl MprocApplication {
@@ -21,6 +23,7 @@ impl MprocApplication {
             state: Rc::new(State::new()),
             settings: AppSettings::default(),
             main_window: MainWindow::new(gtk_app),
+            gtk_application: gtk_app.clone(),
         }
     }
 
@@ -32,27 +35,29 @@ impl MprocApplication {
 
         let commands = extract_all_commands(&args, self.settings.process_limit);
 
-        let state_c = self.state.clone();
         self.main_window
             .on_exit_button_clicked(move |app_window: &ApplicationWindow| {
                 app_window.close();
-                state_c.kill_all_processes_gracefully();
-                info!("mproc stopped. Goodbye!")
             });
+        self.on_application_exit();
 
-        let state_c2 = self.state.clone();
-        let settings_c = self.settings.clone();
+        let state = self.state.clone();
+        let settings = self.settings.clone();
         commands.iter().for_each(|command| {
             let mproc_process_container = self.main_window.create_process_container();
-            spawn_process(
-                mproc_process_container,
-                command.clone(),
-                &state_c2,
-                &settings_c,
-            );
+            spawn_process(mproc_process_container, command.clone(), &state, &settings);
         });
 
         self.main_window.show();
         info!("Application started.")
+    }
+
+    fn on_application_exit(&self) {
+        let state = self.state.clone();
+        self.gtk_application.connect_shutdown(move |_| {
+            info!("Shutting down...");
+            state.kill_all_processes_gracefully();
+            info!("mproc stopped. Goodbye!")
+        });
     }
 }
